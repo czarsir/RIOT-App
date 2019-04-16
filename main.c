dbbba44eba98e63efa9e559c99b0dc1cb81d4b13
Ding <czarsir@gmail.com>
2019-04-17 00:44:02 +0200

    [thread] add a thread for rf215 app.

--- main.c
+++ main.c
@@ -6,15 +6,22 @@
 #include <stdio.h>
 //#include <unistd.h>
 
+/*** System ***/
+#include "thread.h"
+
 /*** Module ***/
 #include "xtimer.h"
 #include "shell.h"
 
 
 /*** Variables ***/
+char rf_stack[THREAD_STACKSIZE_MAIN];
 
 /*** Functions ***/
 int show(int cnt, char **arg);
+/* threads */
+void *thread_handler_rf(void *arg);
+
 
 /*** Special Variables ***/
 const shell_command_t sh_cmd[] = {
@@ -23,12 +30,22 @@ const shell_command_t sh_cmd[] = {
 };
 
 
+/*** Application **********************************************/
+
 int main(void)
 {
     puts("It's a simple app!");
 
+	/*** create thread for rf ***/
+	thread_create(rf_stack, sizeof(rf_stack),
+				  THREAD_PRIORITY_MAIN - 1,
+				  THREAD_CREATE_STACKTEST,
+				  thread_handler_rf,
+				  NULL, "thread rf");
+
+	/*** main process ***/
     printf("Go:\n");
-	
+
 	/*** shell ***/
 //    char line_buf[SHELL_DEFAULT_BUFSIZE];
 //    shell_run(sh_cmd, line_buf, SHELL_DEFAULT_BUFSIZE);
@@ -50,6 +67,22 @@ int main(void)
 	}
 
     return 0;
+}
+
+void *thread_handler_rf(void *arg)
+{
+	(void)arg;
+	msg_t m;
+
+	puts("--->>> Thread rf starts.");
+
+	while(1)
+	{
+		msg_receive(&m);
+		puts("Got one Message.");
+	}
+
+	return NULL;
 }
 
 int show(int cnt, char **arg)
